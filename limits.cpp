0498177819e21b8ee6d3ab92c47b9e332caf4b5f
Tamires Harumi <tamiresharumi@gmail.com>
2011-05-31 21:42:09 -0300

    Gera teto e chão, mas paredes não :/

--- limits.cpp
+++ limits.cpp
@@ -13,6 +13,10 @@ void Limits::setInc(float nInc){
     inc = nInc;
 }
 
+float Limits::getT(void){
+    return T;
+}
+
 int Limits::readFile(const char *filename)
 {
     FILE* f = fopen(filename, "r+");
@@ -36,27 +40,28 @@ int Limits::readFile(const char *filename)
     printf("T|%d\nC|%d\nL|%d\n\n", T, C, L);
     getFloor(0, "p0.obj");
     getFloor(T, "p6.obj");
+    getWall(0,"p3.obj", "p4.obj");
 }
 
 
 int Limits::getFloor(float y, const char* arq){
 
     float i, j;
+
     float px = (L/inc)+1;
     float pz = (C/inc)+1;
-#if TESTE
-    printf("px|%f\npz|%f\n\n", px, pz);
-#endif
 
+
+    printf("px|%f\npz|%f\n\n", px, pz);
     if((fmod(L, inc) != 0)||(fmod(C, inc) != 0)){
         printf("Incremento incompativel!\n");
         return 0;
     }
     FILE* f = fopen(arq, "w+");
     FILE* x = fopen("log_for.log", "w+");
-    for(i=-(L/2.); i<=(L/2.); i+=inc){
-        for(j=-(C/2.); j<=(C/2.); j+=inc){
-            fprintf(f, "v %5.2f %5.2f %5.2f\n", j, y, i);
+    for(i=-(C/2.); i<=(C/2.); i+=inc){
+        for(j=-(L/2.); j<=(L/2.); j+=inc){
+            fprintf(f, "v %5.2f %5.2f %5.2f\n", i, y, j);
         }
     }
     fprintf(f, "vt 0 0\n");
@@ -82,44 +87,70 @@ int Limits::getFloor(float y, const char* arq){
                     (i*px)+(j+1), ((i+1)*px + (j+1)), (i*px)+(j+2));
         }
     }
-//    for(i=0;i<(pz-1);i++){
-//        for(j=1;j<px;j++){
-//            fprintf(f, "f %.0f/4/1 %.0f/1/1 %.0f/3/1\n",
-//                      (i*px)+(j+1), ((i+1)*px + j), ((i+1)*px)+(j+1));
-//        }
-//    }
+    for(i=0;i<(pz-1);i++){
+        for(j=1;j<px;j++){
+            fprintf(f, "f %.0f/4/1 %.0f/1/1 %.0f/3/1\n",
+                      (i*px)+(j+1), ((i+1)*px + j), ((i+1)*px)+(j+1));
+        }
+    }
     fclose(f);
     fclose(x);
     return 1;
 }
 
-//int Limits::getWall(bool xz, float v, const char* arq){
-//
-//    float i, j;
-//    if(xz){  //fixamos z; paredes
-//        float px = (L/inc)+1;
-//        float x;
-//    }
-//    else
-//        float pz = (C/inc)+1;
-//    float py = (T/inc)+1;
-//
-//#if TESTE
-//    printf("px|%f\npz|%f\n\n", px, pz);
-//#endif
-//
-//    if((fmod(L, inc) != 0)||(fmod(C, inc) != 0)){
-//        printf("Incremento incompativel!\n");
-//        return 0;
-//    }
-//    FILE* f = fopen(arq, "w+");
-//    for(i=-(L/2.); i<=(L/2.); i+=inc){
-//        for(j=-(C/2.); j<=(C/2.); j+=inc){
-//            fprintf(f, "v %5.2f %5.2f %5.2f\n", j, y, i);
-//        }
-//    }
-//    fprintf(f, "vt 0 0\n");
-//    fprintf(f, "vt 0 1\n");
-//    fprintf(f, "vt 1 0\n");
-//    fprintf(f, "vt 1 1\n");
-//}
+
+int Limits::getWall(bool xz, const char* arqr, const char *arql){
+
+   float i, j;
+       //TODO verificar arquivo e comparar dados com o programa atual
+    FILE *a = fopen(arqr, "w+");
+    FILE *b = fopen(arql, "w+");
+    if(!xz){
+        getWallX(a, b);
+    }
+    //else getWallZ(v, a);
+    fclose(a);
+    fclose(b);
+}
+
+int Limits::getWallX(FILE* right, FILE *left){
+
+    float i, j;
+    float py = (T/inc)+1;
+    float pz = (C/inc)+1;
+#if TESTE
+    printf("py|%f\npz|%f\n\n", py, pz);
+    printf("= Teste =\nNo quadrado de cima:\ni vai de 0 a %.0f\nj vai de 0 a %.0f\n\n", (py-2), (pz-2));
+#endif
+
+    if((fmod(T, inc) != 0)||(fmod(C, inc) != 0)){
+        printf("Incremento incompativel!\n");
+        return 0;
+    }
+    for(i=T; i>=0; i-=inc){
+        for(j=-(C/2.); j<=(C/2.); j+=inc){
+            fprintf(left, "v %5.2f %5.2f %5.2f\n", -(C/2.), i, j);
+            fprintf(right, "v %5.2f %5.2f %5.2f\n", C/2., i, j);
+        }
+    }
+    fprintf(right, "vt 0 0\n");
+    fprintf(right, "vt 0 1\n");
+    fprintf(right, "vt 1 0\n");
+    fprintf(right, "vt 1 1\n");
+    fprintf(left, "vt 0 0\n");
+    fprintf(left, "vt 0 1\n");
+    fprintf(left, "vt 1 0\n");
+    fprintf(left, "vt 1 1\n");
+
+    fprintf(right, "vn -1 0 0\n");
+    fprintf(left, "vn 1 0 0\n");
+
+    for(i=0;i<py-1;i++){
+        for(j=0;j<pz-1;j++){
+            fprintf(left, "f %.0f/4/1 %.0f/2/1 %.0f/3/1\n",
+                    (pz*i)+(j+1), (i*pz) + j + 2, (i+1)*pz + (j+1));
+            fprintf(right, "f %.0f/2/1 %.0f/1/1 %.0f/4/1\n",
+                    (pz*i)+(j+1), (i+1)*pz + j + 1, (i*pz) + (j+2));
+        }
+    }
+}
--- limits.h
+++ limits.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #ifndef LIMITS_H
 #define LIMITS_H
 
@@ -6,11 +7,12 @@ class Limits
     public:
         Limits();
         void setInc(float nInc);
+        float getT(void);
     private:
         int readFile(const char *filename);
         int getFloor(float y, const char* arq);
-        int getWall(bool xz, float v, const char* arq);
-
+        int getWall(bool xz, const char* arqr, const char *arql);
+        int getWallX(FILE *right, FILE *left);
         float inc;
         int C ,L, T;
         /**
